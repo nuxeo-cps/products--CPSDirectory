142f96d1f9fa2433c21e6359efc80a445e954eed
fguillaume <devnull@localhost>
2003-11-17 19:39:12 +0000

    Untested version that does a search on id, roles, groups.

--- UserFolderPatch.py
+++ UserFolderPatch.py
@@ -23,7 +23,60 @@ Patches User Folder to give it a standard API.
 
 from zLOG import LOG, TRACE
 
+from types import StringType
+
+from Acquisition import aq_base
 from AccessControl.User import UserFolder, User
+
+
+#
+# Helpers
+#
+
+def _preprocessQuery(mapping):
+    """Compute is_list_search and query."""
+    is_list_search = {}
+    query = {}
+    for key, value in mapping.items():
+        if type(value) is StringType:
+            is_list_search[key] = 0
+            query[key] = value.lower()
+        else:
+            is_list_search[key] = 1
+            query[key] = value
+    return is_list_search, query
+
+def _isEntryMatching(entry, is_list_search, query):
+    """Is the entry matching the query?
+
+    Does an AND search for all key, value of the query.
+    If the entry value corresponding to a key is a list,
+    does an OR search on all the list elements.
+    If the query value is a string, does a substring lowercase search.
+    If the query value is a list, does OR search with exact match.
+    """
+    for key, value in query.items():
+        if not value:
+            # Ignore empty searches.
+            continue
+        if not entry.has_key(key):
+            return 0
+        searched = entry[key]
+        if searched is None:
+            return 0
+        if type(searched) is StringType:
+            searched = (searched,)
+        matched = 0
+        for item in searched:
+            if is_list_search[key]:
+                matched = item in value
+            else:
+                matched = item.lower().find(value) != -1
+            if matched:
+                break
+        if not matched:
+            return 0
+    return 1
 
 #
 # New APIs
@@ -31,44 +84,86 @@ from AccessControl.User import UserFolder, User
 
 # User Folder
 
-def searchUsers(self, mapping, attributes=(), **kw):
-    pass
+def searchUsers(self, query={}, props=None, options=None, **kw):
+    """Search for users having certain properties.
+
+    If props is None, returns a list of ids:
+      ['user1', 'user2']
+
+    If props is not None, it must be sequence of property ids. The
+    method will return a list of tuples containing the user id and a
+    dictionary of available properties:
+      [('user1', {'email': 'foo', 'age': 75}), ('user2', {'age': 5})]
+
+    Options is used to specify the search type if possible. XXX
+    """
+    # The basic implementation doesn't know about properties beside
+    # roles and groups.
+    query.update(kw)
+    do_roles = query.has_key('roles')
+    do_groups = query.has_key('groups')
+    is_list_search, query = _preprocessQuery(query)
+    res = {}
+    for user in self.getUsers():
+        base_user = aq_base(user)
+        id = user.getId()
+        entry = {'id': id}
+        if do_roles:
+            entry['roles'] = user.getRoles()
+        if do_groups and hasattr(base_user, 'getGroups'):
+            entry['groups'] = user.getGroups()
+        if not _isEntryMatching(entry, is_list_search, query):
+            continue
+        if props is None:
+            res.append(id)
+        else:
+            d = {}
+            for key in props:
+                if entry.has_key(key):
+                    d[key] = entry[key]
+            res.append((id, d))
+    return res
+
+def listUserProperties(self):
+    """Lists properties settable or searchable on the users."""
+    return ('id', 'roles', 'groups')
 
 def hasUser(self, user_id):
-    pass
+    raise NotImplementedError
 
 userfolder_methods = (
     searchUsers,
-    hasUser,
+    listUserProperties,
+    #hasUser,
     )
 
 # User
 
 def listProperties(self): # ?
-    pass
+    raise NotImplementedError
 
 def hasProperty(self, id):
-    pass
+    raise NotImplementedError
 
 def getProperty(self, id, default=None):
-    pass
+    raise NotImplementedError
 
 def getProperties(self, ids):
-    pass
+    raise NotImplementedError
 
 def setProperty(self, id, value):
-    pass
+    raise NotImplementedError
 
 def setProperties(self, **kw):
-    pass
+    raise NotImplementedError
 
 user_methods = (
-    listProperties,
-    hasProperty,
-    getProperty,
-    getProperties,
-    setProperty,
-    setProperties,
+    #listProperties,
+    #hasProperty,
+    #getProperty,
+    #getProperties,
+    #setProperty,
+    #setProperties,
     )
 
 #
