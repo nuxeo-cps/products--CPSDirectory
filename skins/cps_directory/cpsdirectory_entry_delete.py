3ac24aa00b1523af0884b273acf7b29767cd3349
fguillaume <devnull@localhost>
2003-12-08 20:54:17 +0000

    Better delete skin action.

--- skins/cps_directory/cpsdirectory_entry_delete.py
+++ skins/cps_directory/cpsdirectory_entry_delete.py
@@ -1,6 +1,11 @@
-##parameters=dirname, id, REQUEST
+##parameters=dirname, id, REQUEST=None
 
 dir = context.portal_directories[dirname]
 dir.deleteEntry(id)
 
-return dir.cpsdirectory_entry_search_form(dirname=dirname)
+if REQUEST is not None:
+    portal_url = context.portal_url()
+    psm = 'psm_entry_deleted'
+    REQUEST.RESPONSE.redirect('%s/cpsdirectory_entry_search_form?dirname=%s'
+                              '&portal_status_message=%s' %
+                              (portal_url, dirname, psm))
