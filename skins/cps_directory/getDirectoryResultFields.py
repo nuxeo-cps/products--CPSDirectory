06cfedb0c5d100b05994d703cb5e546601cc1330
bdelbosc <devnull@localhost>
2004-01-28 16:30:58 +0000

    customize result for members

--- skins/cps_directory/getDirectoryResultFields.py
+++ skins/cps_directory/getDirectoryResultFields.py
@@ -5,6 +5,14 @@ Get the list of fields to be displayed in the search result list.
 Format is [{'id': 'sn', 'title': 'Name'}, ] title will be i18n.
 """
 fields = context.getCustomDirectoryResultFields(directory_id, default_field)
+
+if not fields:
+    if directory_id == 'members':
+        fields = [{'id': 'sn', 'title': 'label_last_name'},
+                  {'id': 'givenName', 'title': 'label_first_name'},
+                  {'id': 'id', 'title': 'label_user_name'},
+                  ]
+
 if not fields:
     fields = [{'id': default_field,
                'title': 'Id'}]
