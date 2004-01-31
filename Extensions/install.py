95ef400d492be0a3dc52e1ba539899b6e657deef
sfermigier <devnull@localhost>
2004-01-31 10:20:18 +0000

    Fixing installtion fiasco.

--- Extensions/install.py
+++ Extensions/install.py
@@ -218,5 +218,10 @@ def install(self):
         permission='View', category='global', visible=1)
 
 
+    # Synchronization from members directory schema to MemberData properties
+    mdir = portal.portal_directories.members
+    mdir.updateMemberDataFromSchema()
+    
+
     pr("End of specific CPSDirectory install")
     return pr('flush')
