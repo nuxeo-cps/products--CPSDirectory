31abe13239c12a89e0e2bb2ddd89ed22086c61a3
bdelbosc <devnull@localhost>
2004-02-24 09:25:49 +0000

    comment processing of field

--- skins/cps_directory/getCustomDirectoryResultFields.py
+++ skins/cps_directory/getCustomDirectoryResultFields.py
@@ -4,9 +4,14 @@ Get the list of fields to be displayed in the directory search result list.
 
 This file should be override in your product
 
-expected list format [{'id': 'sn', 'title': 'Name', 'sort': 'asc'}, ]
- 'sort' is optional accepted value for sort are 'asc' or 'desc'
+expected list format [{'id': 'sn',
+                       'title': 'Name',
+                       'sort': 'asc',
+                       'process': 'my_custom_sn_process'}, ]
  'title' will be i18n.
+ 'sort' is optional accepted value for sort are 'asc' or 'desc'
+ 'process' is optional, use the method name to process value before
+           sorting, the new value = meth(field_id, value)
 
 example:
 if directory_id == 'ldap_people':
