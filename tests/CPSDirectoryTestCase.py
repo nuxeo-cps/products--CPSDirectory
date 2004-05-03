329068d49c8117b9f88204eb8b08b4fd056ae794
fguillaume <devnull@localhost>
2004-05-03 18:01:36 +0000

    Added code to be able to see LOG errors on the console.

--- tests/CPSDirectoryTestCase.py
+++ tests/CPSDirectoryTestCase.py
@@ -10,7 +10,27 @@ ZopeTestCase.installProduct('CPSDirectory')
 
 CPSTestCase.setupPortal()
 
+import zLOG
+
+def print_log_errors(min_severity=zLOG.INFO):
+    if hasattr(zLOG, 'old_log_write'):
+        return
+    def log_write(subsystem, severity, summary, detail, error,
+                  PROBLEM=zLOG.PROBLEM, min_severity=min_severity):
+        if severity >= min_severity:
+            print "%s(%s): %s, %s" % (subsystem, severity, summary, detail)
+    zLOG.old_log_write = zLOG.log_write
+    zLOG.log_write = log_write
+
+def ignore_log_errors():
+    if hasattr(zLOG, 'old_log_write'):
+        zLOG.log_write = zLOG.old_log_write
+        del zLOG.old_log_write
+
 class CPSDirectoryTestCase(CPSTestCase.CPSTestCase):
-    pass
 
+    def afterSetUp(self):
+        print_log_errors(zLOG.WARNING)
 
+    def beforeTearDown(self):
+        ignore_log_errors()
