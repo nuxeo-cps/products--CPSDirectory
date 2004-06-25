ed8c24cf9df6eaca67ea2d5453f735700bfa4150
edegoute <devnull@localhost>
2004-06-25 13:58:36 +0000

    use new generic labels

--- skins/cps_directory/getDirectoryVocabularies.py
+++ skins/cps_directory/getDirectoryVocabularies.py
@@ -22,8 +22,8 @@ global_roles_vocabulary = {
     'type': 'CPS Vocabulary',
     'data': {
         'tuples': (
-            ('Member', "Member", 'label_roles_Member'),
-            ('Manager', "Manager", 'label_roles_Manager'),
+            ('Member', "Member", 'label_cpsdir_roles_Member'),
+            ('Manager', "Manager", 'label_cpsdir_roles_Manager'),
             )
         },
     }
