292e4439b844ebdd3d7c5f8fa9b0f959ffa36868
fguillaume <devnull@localhost>
2004-11-05 18:10:39 +0000

    Correctly quote url args.

--- skins/cps_directory/cpsdirectory_entry_created.py
+++ skins/cps_directory/cpsdirectory_entry_created.py
@@ -8,13 +8,16 @@ The context is the directory.
 May return a rendered document, or do a redirect.
 """
 
+from urllib import urlencode
+
 dirname = context.getId()
 id_field = context.id_field
 id = datastructure.getDataModel()[id_field]
 
 portal_url = context.portal_url()
-action_path = 'cpsdirectory_entry_edit_form?dirname=%s&id=%s' % (
-    dirname, id)
-psm = 'psm_entry_created'
-context.REQUEST.RESPONSE.redirect('%s/%s&portal_status_message=%s' %
-                                  (portal_url, action_path, psm))
+args = urlencode({'dirname': dirname,
+                  'id': id,
+                  'portal_status_message': 'psm_entry_created',
+                  })
+action_path = 'cpsdirectory_entry_edit_form?'+args
+context.REQUEST.RESPONSE.redirect('%s/%s' % (portal_url, action_path))
