570ec1792395881db0911dd8ae2f4e578f62cda3
madarche <devnull@localhost>
2007-04-20 12:26:48 +0000

    - Cosmit and little optims.

--- skins/cps_directory/setDirectoryBreadCrumbs.py
+++ skins/cps_directory/setDirectoryBreadCrumbs.py
@@ -8,28 +8,28 @@ from Products.CMFCore.utils import getToolByName
 
 utool = getToolByName(context, 'portal_url')
 portal = utool.getPortalObject()
+portal_url = portal.absolute_url()
 
 portal_root_breadcrumb = {
     'id': portal.getId(),
-    'url': portal.absolute_url(),
+    'url': portal_url,
     'title': portal.Title(),
     }
 
 directories_breadcrumb = {
     'id': 'directories',
-    'url': context.portal_url() + '/cpsdirectory_view',
+    'url': portal_url + '/cpsdirectory_view',
     'title': context.translation_service('Directories'),
-    }
-
-directory_breadcrumbs = {
-    'id': 'directories',
-    'url': context.portal_url() + '/cpsdirectory_entry_search_form?dirname=' + dirname,
-    'title': context.translation_service(dirtitle),
     }
 
 breadcrumb_set = [portal_root_breadcrumb, directories_breadcrumb]
 
 if dir.isVisible():
+    directory_breadcrumbs = {
+        'id': 'directories',
+        'url': portal_url + '/cpsdirectory_entry_search_form?dirname=' + dirname,
+        'title': context.translation_service(dirtitle),
+        }
     breadcrumb_set.append(directory_breadcrumbs)
 
 REQUEST.set('breadcrumb_set', breadcrumb_set)
